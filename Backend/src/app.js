3740d6df84b146a741631b7dd446e6f0c8822bc0
Jay Jadav <110678867+Jayjadav221024@users.noreply.github.com>
2026-06-30 12:59:45 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cookieparser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:4000", "http://localhost:5000"],
+  origin: ["http://localhost:5173", "http://localhost:4000", "http://localhost:5000","https://zoominsta-2.onrender.com"],
   credentials: true,
 }));
 
